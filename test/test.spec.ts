be15ee3b39508a098d14520e5605bcf7d858b749
Young Rok Kim <rok0810@gmail.com>
2017-07-11 17:03:24 +0900

    add test for addLocaleData

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -3,63 +3,120 @@ import * as Joi from 'joi';
 import { expect } from 'chai';
 
 describe('joi-i18n', () => {
+  describe('addLocaleData', () => {
+    it('should throw error under invalid locale code', () => {
+      expect(() => {
+        Joi.addLocaleData('!!invalid_character!!', { errors: {} });
+      }).to.throw('"locale" must only contain alpha-numeric and underscore characters');
+    });
 
-  before(() => {
-    Joi.addLocaleData('ko_KR', {
-      errors: {
-        number: {
-          base: '은(는) 숫자 형태여야 합니다',
-          required: '!!{{key}}가 정의되지 않았습니다.'
+    it('should throw error under invalid language object', () => {
+      expect(() => {
+        Joi.addLocaleData('test_locale_1', { errors: null });
+      }).to.throw('"errors" must be an object');
+    });
+
+    it('should throw error under invalid language error descriptor (number provided)', () => {
+      expect(() => {
+        Joi.addLocaleData('test_locale_2', { errors: { any: { base: 1 } } as any });
+      }).to.throws(`"base" must be a string`);
+    });
+
+    it('should throw error under invalid language error descriptor (number provided for errors.key)', () => {
+      expect(() => {
+        Joi.addLocaleData('test_locale_3', { errors: { key: 1 } as any });
+      }).to.throws(`"key" must be a string`);
+    });
+
+    it('should not throw error under valid arguments', () => {
+      expect(() => {
+        Joi.addLocaleData('test_locale_4', {
+          errors: {
+            any: {
+              base: '!!Good',
+              required: (error) => `It's OK to register a function`
+            }
+          }
+        });
+      }).to.not.throw();
+    });
+  });
+
+  describe('validate', () => {
+    before(() => {
+      Joi.addLocaleData('ko_KR', {
+        errors: {
+          number: {
+            base: (error) => `"${error.path}" 은(는) 숫자 형태여야 합니다`,
+            required: '!!{{key}}가 정의되지 않았습니다.'
+          }
         }
-      }
+      });
+      Joi.addLocaleData('custom_key_locale', {
+        errors: {
+          key: '[{{key}}] '
+        }
+      });
+    })
+
+    const schema = Joi.object({
+      number: Joi.number().required(),
+      string: Joi.string()
     });
-    Joi.addLocaleData('custom_key_locale', {
-      errors: {
-        key: '[{{key}}] '
-      }
+
+    it('should format schema.validate() with valid locale option', () => {
+      const { error } = schema.validate({ number: 'string' }, { locale: 'ko_KR' });
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `"number" 은(는) 숫자 형태여야 합니다`);
     });
-  })
 
-  const schema = Joi.object({
-    number: Joi.number().required(),
-    string: Joi.string()
-  });
+    it('should format schema.options() with valid locale option', () => {
+      const { error } = schema.options({ locale: 'ko_KR' }).validate({ number: 'string' });
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `"number" 은(는) 숫자 형태여야 합니다`);
+    });
 
-  it('schema.validate() with valid locale option', () => {
-    const { error } = schema.validate({ number: 'string' }, { locale: 'ko_KR' });
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`"number" 은(는) 숫자 형태여야 합니다`);
-  });
+    it('should format Joi.validate() with valid locale option', () => {
+      const { error } = Joi.validate({ number: 'string' }, schema, { locale: 'ko_KR' });
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `"number" 은(는) 숫자 형태여야 합니다`);
+    });
 
-  it('schema.options() with valid locale option', () => {
-    const { error } = schema.options({ locale: 'ko_KR' }).validate({ number: 'string' });
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`"number" 은(는) 숫자 형태여야 합니다`);
-  });
+    it('should format Joi.validate() to schema.options() with valid locale option', () => {
+      const { error } = Joi.validate({ number: 'string' }, schema.options({ locale: 'ko_KR' }));
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `"number" 은(는) 숫자 형태여야 합니다`);
+    });
 
-  it('Joi.validate() with valid locale option', () => {
-    const { error } = Joi.validate({ number: 'string' }, schema, { locale: 'ko_KR' });
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`"number" 은(는) 숫자 형태여야 합니다`);
-  });
+    it('should format Joi.validate() with custom key option', () => {
+      const { error } = Joi.validate({ number: 'string' }, schema.options({ locale: 'custom_key_locale' }));
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `[number] must be a number`);
+    });
 
-  it('Joi.validate() to schema.options() with valid locale option', () => {
-    const { error } = Joi.validate({ number: 'string' }, schema.options({ locale: 'ko_KR' }));
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`"number" 은(는) 숫자 형태여야 합니다`);
-  });
+    before(() => Joi.setDefaultLocale('ko_KR'));
+    after(() => Joi.setDefaultLocale(null));
 
-  it('Joi.validate() with custom key option', () => {
-    const { error } = Joi.validate({ number: 'string' }, schema.options({ locale: 'custom_key_locale' }));
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`[number] must be a number`);
-  });
+    it('should format Joi.validate() over Joi.setDefaultLocale() first', () => {
+      const { error } = schema.validate({ number: 'string' });
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `"number" 은(는) 숫자 형태여야 합니다`);
+    });
 
-  after(() => Joi.setDefaultLocale(null))
-  it('Joi.validate() with Joi.setDefaultLocale()', () => {
-    Joi.setDefaultLocale('ko_KR');
-    const { error } = schema.validate({ number: 'string' });
-    expect(error).to.exist;
-    expect(error.details[0].message).to.equals(`"number" 은(는) 숫자 형태여야 합니다`);
+    it('should format Joi.validate() over provided options.language object first', () => {
+      const options = {
+        language: {
+          errors: { number: { base: '!!is it number?' } }
+        }
+      };
+      const { error } = schema.validate({ number: 'string' }, options);
+      expect(options).to.deep.equals({
+        language: {
+          errors: { number: { base: '!!is it number?' } }
+        }
+      });
+      expect(error).to.exist;
+      expect(error).to.have.nested.property('details[0].message', `is it number?`);
+    });
   });
 });
